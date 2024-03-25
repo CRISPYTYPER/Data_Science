17bd8edb090df341d856e377e342bcb619d66261
CRISPYTYPER <jack3409@naver.com>
2024-03-26 01:14:39 +0900

    apriori part (step 1) done. but not sure the logic is perfectly working(needs to be reviewed). also encapsulated the step1 part

--- hw1/2019040591_박진수_hw1.py
+++ hw1/2019040591_박진수_hw1.py
@@ -1,25 +1,17 @@
 import sys
 
-if __name__ == '__main__':
-    # Argument num checking
-    if len(sys.argv) != 4:
-        print("<ERROR> Please input three arguments!")
-        sys.exit()
-    minimum_support_str = sys.argv[1]  # 10(%)
-    input_file_name = sys.argv[2]  # input.txt
-    output_file_name = sys.argv[3]  # output.txt
+def get_frequent_itemset_list(transactions) :
+    """
+        Generates frequent itemsets from transactional data using the Apriori algorithm.
 
-    # Open the file to read
-    with open(input_file_name, 'r') as file:
-        # Initialize empty list to store transactions
-        transactions = []
+        Parameters:
+        transactions (list): A list of transactions(DB), where each transaction is represented as a list of items.
+        Be aware that each element in a transaction is string (ex. '7')
 
-        # Iterate over each line to read each transaction(item_id's)
-        for line in file:
-            # Split the line into item IDs with ('\t') as a delimiter
-            item_ids = line.strip().split('\t')
-            # Add each transaction into the list "transactions"
-            transactions.append(item_ids)
+        Returns:
+        list: A list of frequent itemsets discovered from the transactional data.
+        ex) [{(16,): 212, (3,): 150, (8,): 226}, {(8, 16): 151}]
+    """
 
     # number of transactions (used to calculate minimum support)
     num_of_transactions = len(transactions)
@@ -27,7 +19,6 @@ if __name__ == '__main__':
     # minimum support (counts) -> 500 * 10% = 50.0
     minimum_support = int(minimum_support_str) / 100 * num_of_transactions
 
-    # 'transactions' variable is now such like [['7', '14'], ['9'], ['18', '2', '4', '5', '1']]
     frequent_itemset_list = []  # list of dictionaries, each element in index i has itemset of length (i+1)
 
     # For size 1 itemset, make C1 (candidates)
@@ -54,15 +45,15 @@ if __name__ == '__main__':
     for key in keys_to_delete:
         del itemset_count_dict[key]
     frequent_itemset_list.append(itemset_count_dict)  # frequent_item_list[0] -> size 1 itemset
-    print(frequent_itemset_list[0])
+    # print(frequent_itemset_list[0])
     # K = 1 done (made L_1)
 
     k = 1  # variable for iteration
     while True:
         # Make candidates (C_k+1) generated from L_k
         candidate_itemsets = set()
-        for itemset_1 in frequent_itemset_list[k-1]:  # itemset_1 -> ('9',)
-            for itemset_2 in frequent_itemset_list[k-1]:  # itemset_1 -> ('9',)
+        for itemset_1 in frequent_itemset_list[k - 1]:  # itemset_1 -> ('9',)
+            for itemset_2 in frequent_itemset_list[k - 1]:  # itemset_1 -> ('9',)
                 # change tuple into set
                 itemset_1 = set(itemset_1)
                 itemset_2 = set(itemset_2)
@@ -77,8 +68,6 @@ if __name__ == '__main__':
                 if len(union_set_list) == k + 1:
                     candidate_itemsets.add(tuple(union_set_list))
 
-
-        # TODO: We need to remove the candidates in the set if the subset of each itemset is not frequent
         # 한국말로 써놓자면, L_k-1을 이용해서 합집합을 계산하여 C_k의 후보군을 구해놓았지만,
         # apriori 알고리즘의 핵심인, L_k-1에서 frequent하지 않은 것이 확인되었다면 이후에도 그것의 superset은 고려할 필요가 없다는 부분을 구현해야함.
         # keys_to_delete 라는 리스트에 이전 step에서 구한 frequent 하지 않은 itemset를 저장해 두었음
@@ -88,22 +77,23 @@ if __name__ == '__main__':
             # frequent_itemset_list[k-1] == {(7,): 120, (14,): 128, (9,): 139}
             for itemset_to_delete in keys_to_delete:  # iterating keys_to_delete (ex. (6, ))
                 if set(itemset_to_delete).issubset(set(itemset)):
-                    pruned_candidate_itemsets.discard(itemset)  # remove the item if its subset is not frequent(apriori algorithm)
+                    pruned_candidate_itemsets.discard(
+                        itemset)  # remove the item if its subset is not frequent(apriori algorithm)
 
-        print(len(pruned_candidate_itemsets))
-
+        # print(len(pruned_candidate_itemsets))
 
         # 이제 pruned_condidate_itemsets에 pruned 완료된 candidates가 담겨있음.
         # 이제 DB-scan 작업 후, frequent하지 않은 itemset은 없애기
-
 
         # DB scan (DB: transactions)
         # count the support in DB
         itemset_count_dict = dict()  # itemset_count_dict = {}
         for transaction in transactions:  # loop the DB to get frequent 1-itemset
-            transaction_set = set([int(item) for item in transaction])  # transform the string list into the set of integers
+            transaction_set = set(
+                [int(item) for item in transaction])  # transform the string list into the set of integers
             for pruned_itemset in pruned_candidate_itemsets:
-                if set(pruned_itemset).issubset(transaction_set):  # if the pruned_itemset is the subset of transaction (frequency += 1)
+                if set(pruned_itemset).issubset(
+                        transaction_set):  # if the pruned_itemset is the subset of transaction (frequency += 1)
                     if pruned_itemset not in itemset_count_dict:  # if the itemset is not found as a key
                         itemset_count_dict[pruned_itemset] = 1  # add the key and set the count to 1
                     else:  # if the itemset is already in the dictionary
@@ -125,9 +115,40 @@ if __name__ == '__main__':
 
         frequent_itemset_list.append(itemset_count_dict)  # frequent_item_list[k] -> size k+1 itemset
         k += 1  # same as i++ in for-loop
-    # for test
-    for frequent_itemset in frequent_itemset_list:
-        print(frequent_itemset)
+    # # for test
+    # for frequent_itemset in frequent_itemset_list:
+    #     print(frequent_itemset)
+    return frequent_itemset_list  # ex) [{(16,): 212, (3,): 150, (8,): 226}, {(8, 16): 151}]
+
+if __name__ == '__main__':
+    # Argument num checking
+    if len(sys.argv) != 4:
+        print("<ERROR> Please input three arguments!")
+        sys.exit()
+    minimum_support_str = sys.argv[1]  # 10(%)
+    input_file_name = sys.argv[2]  # input.txt
+    output_file_name = sys.argv[3]  # output.txt
+
+    # Open the file to read
+    with open(input_file_name, 'r') as file:
+        # Initialize empty list to store transactions
+        transactions = []
+
+        # Iterate over each line to read each transaction(item_id's)
+        for line in file:
+            # Split the line into item IDs with ('\t') as a delimiter
+            item_ids = line.strip().split('\t')
+            # Add each transaction into the list "transactions"
+            transactions.append(item_ids)
+
+
+    # 'transactions' variable is now such like [['7', '14'], ['9'], ['18', '2', '4', '5', '1']]
+    # get frequent itemset list using apriori algorithm. Index 0 refers to L_1
+    frequent_itemset_list = get_frequent_itemset_list(transactions)  # frequent_itemset_list == [{(16,): 212, (3,): 150, (8,): 226}, {(8, 16): 151}]
+    print(frequent_itemset_list)
+    # TODO : <step 2: for each frequent itemset, find association rules>
+
+
 
 
 
