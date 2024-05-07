b2f75b48ec0aebae82a4efc0f7fcb23efdf96fde
CRISPYTYPER <jack3409@naver.com>
2024-05-07 13:52:31 +0900

    all done assignment2

--- hw2/2019040591_박진수_hw2.py
+++ hw2/2019040591_박진수_hw2.py
@@ -11,7 +11,7 @@ def load_data(filename):
             filename (str): File name to read.
                 ex. "dt_train.txt"
         Return:
-            examples (dict): A list of dictionaries of each training data.
+            examples (list): A list of dictionaries of each example.
                 ex. [{'age': '<=30', 'income': 'high', 'student': 'no', 'credit_rating': 'fair', 'Class:buys_computer': 'no'},...]
             feature_attribute_names (list): A list of feature names (except target).
                 ex. ['age', 'income', 'student', 'credit_rating']
@@ -27,14 +27,40 @@ def load_data(filename):
 
 
 def entropy(examples, target_attribute_name):
-    target_val_counts = Counter(example[target_attribute_name] for example in examples)
+    """
+        Return the entropy of the examples, using target_attribute_name
+
+        Parameters:
+            examples (list): A list of dictionaries of each example
+                ex. [{'age': '<=30', 'income': 'high', 'student': 'no', 'credit_rating': 'fair', 'Class:buys_computer': 'no'},...]
+            target_attribute_name (str): The name of target attribute
+                ex. "Class:buys_computer"
+        Return:
+            (float): The entropy value.
+
+    """
     examples_len = len(examples)
+    target_val_counts = Counter(example[target_attribute_name] for example in examples)
     return -sum((count / examples_len) * math.log2(count / examples_len) for count in target_val_counts.values())
 
 
 def information_gain(examples, attribute_name, target_attribute_name):
-    original_entropy = entropy(examples, target_attribute_name)
+    """
+        Return the information gain of the examples. (Info(D) - Info_A(D))
+
+        Parameters:
+            examples (list): A list of dictionaries of each example
+                ex. [{'age': '<=30', 'income': 'high', 'student': 'no', 'credit_rating': 'fair', 'Class:buys_computer': 'no'},...]
+            attribute_name (str): The name of attribute chosen for branching.
+                ex. "age"
+            target_attribute_name (str): The name of target attribute
+                ex. "Class:buys_computer"
+        Return:
+            (float): The entropy value.
+
+    """
     branches = defaultdict(list)
+    original_entropy = entropy(examples, target_attribute_name)
     for example in examples:
         branches[example[attribute_name]].append(example)
     after_entropy = sum((len(examples_in_branch) / len(examples)) * entropy(examples_in_branch, target_attribute_name) for examples_in_branch in branches.values())
@@ -42,30 +68,70 @@ def information_gain(examples, attribute_name, target_attribute_name):
 
 
 def split_information(examples, attribute_name):
-    counts = Counter(example[attribute_name] for example in examples)
+    """
+        Return the split information of the examples.
+
+        Parameters:
+            examples (list): A list of dictionaries of each example
+                ex. [{'age': '<=30', 'income': 'high', 'student': 'no', 'credit_rating': 'fair', 'Class:buys_computer': 'no'},...]
+            attribute_name (str): The name of attribute chosen for branching.
+                ex. "age"
+        Return:
+            (float): The split information value.
+
+    """
     examples_len = len(examples)
+    counts = Counter(example[attribute_name] for example in examples)
     return -sum((count / examples_len) * math.log2(count / examples_len) for count in counts.values())
 
 
 def gain_ratio(examples, attribute_name, target_attribute_name):
+    """
+        Return the gain ratio of the examples. (Gain(A) - SplitInfo_A(D))
+
+        Parameters:
+            examples (list): A list of dictionaries of each example
+                ex. [{'age': '<=30', 'income': 'high', 'student': 'no', 'credit_rating': 'fair', 'Class:buys_computer': 'no'},...]
+            attribute_name (str): The name of attribute chosen for branching.
+                ex. "age"
+            target_attribute_name (str): The name of target attribute
+                ex. "Class:buys_computer"
+        Return:
+            (float): The gain_ratio.
+
+    """
     info_gain = information_gain(examples, attribute_name, target_attribute_name)
     split_info = split_information(examples, attribute_name)
-    if split_info != 0:
+    if split_info != 0 and info_gain != 0:
         return info_gain / split_info
     else:
         return 0
 
 
 def build_tree(examples, feature_attribute_names, target_attribute_name):
+    """
+        Build a decision tree recursively. It can be a dictionary or a string.
+
+        Parameters:
+            examples (list): A list of dictionaries of each example
+                ex. [{'age': '<=30', 'income': 'high', 'student': 'no', 'credit_rating': 'fair', 'Class:buys_computer': 'no'},...]
+            feature_attribute_names (list): A list of feature attribute names
+                ex. ["age", "income", ...]
+            feature_attribute_name (str): The name of target attribute
+                ex. "Class:buys_computer"
+        Return:
+            (dict) or (str): The node of the decision tree
+
+    """
     distinct_targets = set(example[target_attribute_name] for example in examples)
     if len(distinct_targets) == 1:
         return distinct_targets.pop()
     elif len(feature_attribute_names) == 0:  # no more attributes to compare
         return Counter(example[target_attribute_name] for example in examples).most_common(1)[0][0]  # Return most common target attribute
     best_attribute_name = max(feature_attribute_names, key=lambda attribute: gain_ratio(examples, attribute, target_attribute_name))
-    tree = {best_attribute_name: {}}
-    remaining_attribute_names = [attr for attr in feature_attribute_names if attr != best_attribute_name]
+    remaining_attribute_names = [attribute for attribute in feature_attribute_names if attribute != best_attribute_name]
     branched_examples = defaultdict(list)  # list of branched examples separated by "best_attribute_name"
+    tree = {best_attribute_name: {}}
     for example in examples:
         branched_examples[example[best_attribute_name]].append(example)
     for best_attribute_value, examples_in_one_branch in branched_examples.items():
@@ -74,10 +140,20 @@ def build_tree(examples, feature_attribute_names, target_attribute_name):
 
 
 def classify(tree, example):
-    if type(tree) is not dict:
-        return tree
-        # leaf nodes are represented by their class label
-        # instead of further dictionary structures
+    """
+        Classify the given example using a decision tree.
+
+        Parameters:
+            tree (dict or str): Recursive form of decision tree. A leaf is string type.
+            example(dict): An example of a data.
+                ex. {'age': '<=30', 'income': 'high', 'student': 'no', 'credit_rating': 'fair', 'Class:buys_computer': 'no'}
+        Return:
+            (str): Result of the classification of the given data.
+            ex. "fair"
+
+    """
+    if type(tree) is str:
+        return tree  # leaf nodes are represented by their class label (str)
 
     branching_attribute_name = list(tree.keys())[0]
     attribute_value = example[branching_attribute_name]
